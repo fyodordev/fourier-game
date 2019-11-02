3562e7437643e05c4d0612670ec7bd8f0251c0e8
Pascal Noser <noser.pascal95@gmail.com>
2019-11-02 20:56:58 +0100

    Added _init function to BaseStick scene

--- BaseStick.gd
+++ BaseStick.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 export var speed = 1
 
+func _init(new_speed, new_length):
+	speed = new_speed
+	get_node("Stick").position[0] *= new_length
+	get_node("Junction").position[0] = get_node("Stick").position[0]
+	get_node("Stick/CollisionShape2D").scale[0] *= new_length
+	get_node("Stick/Stick_Sprite").scale[0] *= new_length
+
 func _physics_process(delta):
 	# If paretn node classe == "Node" the parent is the "World" node, i.e. we don't want to get the parent's speed
 	if get_parent().get_class() == "Node":
---
+++ Junction.gd
@@ -0,0 +1,4 @@
+extends KinematicBody2D
+
+func _init():
+	position[0] = get_parent().position[0]*1
