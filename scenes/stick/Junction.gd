d0ce8ea068a2e482cf9fa7ae5a664921ba89e232
sajanth <sajanth@hotmail.com>
2019-11-05 13:43:11 +0100

    Improved visuals for impact on obstacle

--- scenes/stick/Junction.gd
+++ scenes/stick/Junction.gd
@@ -11,9 +11,20 @@ func _ready():
 
 
 func _physics_process(delta):
+	#collison behaviour
 	if move_and_collide(velocity):
+		
+		# make last junction's movement independant from parent 
+		global_position #I think this line transforms position coordinates from relative to the global coordinate system (without it the next line puts the junction back near to the origin, probably because its coordinates would be relative to the parent)
+		set_as_toplevel(true) #removes parent node transformational dependence
+		
+		#animation
 		get_node("Junction_Sprite").set_texture(null) #after collision has been detected remove sprite of last junction
+		get_node("Explosion/Particles2D").set_as_toplevel(true)#necessary that coordinatesystem for particle effects like gravity refers to the global coordinatesystem
+		get_node("Explosion/Particles2D").global_position=global_position #necessary that coordinatesystem for particle effects like gravity refers to the global coordinatesystem
 		get_node("Explosion/Particles2D").emitting=true #initate "explosion" when collisoin has been detected
+		
+		#deathscreen
 		var animP=get_tree().get_root().get_node("World/DeathFade/AnimationPlayer")
 		animP.play("NewFade")
 		yield(animP,"animation_finished")
