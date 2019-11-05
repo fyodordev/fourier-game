d7fdc32ec321405ddfb964eff1310423d718e33c
Pascal Noser <noser.pascal95@gmail.com>
2019-11-05 15:43:31 +0100

    Added sound effects to the start screen UI

--- scenes/main/counter.gd
+++ scenes/main/counter.gd
@@ -1,6 +1,9 @@
 extends Control
 
 var boool=false
+onready var audio_ui_up = get_tree().get_root().get_node("Node2D/ui_up_sound")
+onready var audio_ui_down = get_tree().get_root().get_node("Node2D/ui_down_sound")
+
 func is_on(boool):
 	if not boool:
 		get_node("number_sticks").add_color_override("font_color", Color(0.6,0.6,0.6,1))
@@ -12,9 +15,11 @@ func is_on(boool):
 
 		
 func _on_number_down_pressed():
+	audio_ui_down.play()
 	get_node("number_sticks").text=str(int(get_node("number_sticks").text)-1)
 	pass
 func _on_number_up_pressed():
+	audio_ui_up.play()
 	get_node("number_sticks").text=str(int(get_node("number_sticks").text)+1)
 	pass
 func _process(delta):
--- scenes/start_screen/titlescreen.gd
+++ scenes/start_screen/titlescreen.gd
@@ -7,7 +7,12 @@ func _init():
 		get_node("Vel/Node2D%s"%i).text=global.speeds[i]
 		get_node("Siz/Node2D%s"%i).text=global.lengths[i]
 """
+# Get reference to audio players
+onready var audio_ui_up = get_node("ui_up_sound")
+onready var audio_ui_down = get_node("ui_down_sound")
+
 func _on_number_down_pressed():
+	audio_ui_down.play()
 	print(int(get_node("Label/number_sticks").text))
 	if get_node("Label/number_sticks").text!="1":
 		get_node("Vel/Node2D%s"%get_node("Label/number_sticks").text).boool=false
@@ -16,6 +21,7 @@ func _on_number_down_pressed():
 		global.number_of_sticks-=1
 	pass
 func _on_number_up_pressed():
+	audio_ui_up.play()
 	print(int(get_node("Label/number_sticks").text))
 	if get_node("Label/number_sticks").text!="4":
 		get_node("Label/number_sticks").text=str(int(get_node("Label/number_sticks").text)+1)
